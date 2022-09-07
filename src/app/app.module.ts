1b505060912e2d2098bd94c2b70c698b6f750b5e
LuanCalais <luancalais@gmail.com>
2022-09-07 16:44:27 -0300

    configuar inicio de app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modules
+import { HomeModule } from './modules/home/home.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Import do modulo 'home' e seus componentes
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
